290a4880c7e3b1c863d51192c9195dcd2b9fc5f6
MaxiM <maximgt540@gmail.com>
2017-11-29 16:51:44 +0100

    set error

--- AVLdict/Dictionary.cs
+++ AVLdict/Dictionary.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace AVLdict
 {
-    class Dictionary
+    class Dictionary : Node
     {
+        public Node Root { get; private set; }
+
+        public void Add(string word)
+        {
+            Node newNode = new Node(word);
+            AddToTree(Root, newNode);
+        }
+
+        private void AddToTree(Node parent, Node newNode)
+        {
+            if (Root == null)
+            {
+                Root = newNode;
+                return;
+            }
+
+            if (newNode.Word.CompareTo(parent.Word) < 0)
+            {
+                if (parent.Left == null)
+                {
+                    parent.Left = newNode;
+                }
+            }
+        }
     }
 }
