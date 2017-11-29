5eb22a58c12e456104693e326b48d0863f157553
MaxiM <maximgt540@gmail.com>
2017-11-29 19:17:20 +0100

    create PrintTree method

--- AVLdict/Dictionary.cs
+++ AVLdict/Dictionary.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 
 namespace AVLdict
 {
+    class Constants
+    {
+        public const int MaxTreeDisplay = 128;
+        public const int MaxTreeDisplayPower = 7;
+    }
     class Dictionary
     {
         public Node Root { get; private set; }
@@ -52,20 +57,46 @@ namespace AVLdict
 
         public void PrintTree(Node root)
         {
-            Console.WriteLine("START");
-            if (root == null)
-            {
-                Console.WriteLine(" ");
-                Console.WriteLine("END");
-                return;
-            }
+            string[] Tree = new string[Constants.MaxTreeDisplay+1];
+            int level = MapTree(root, Tree, 1);
 
-            Console.WriteLine(root.Word);
-            Node left= root.Left, right = root.Right;
-            while (left != null && right != null)
+            Console.WriteLine(level);
+
+            int power = Constants.MaxTreeDisplayPower;
+            while ((level >> power) == 0) --power;
+
+            Console.WriteLine(power);
+            level = ((level >> power) << power);
+            int spaces = level / 2;
+
+            for (int i = 1; i < level; i *= 2)
             {
-                break;
+                for (int k = 0; k < (level / i) / 2; k++) { Console.Write(" "); }
+
+                for (int j = i; j < i * 2; j++)
+                {
+                    for (int k = 0; k < level / i / 2; k++) { Console.Write("  "); }
+
+                    if (Tree[j] == null) Tree[j] = "x";
+                    Console.Write(Tree[j] + " ");
+                }
+                spaces /= 2;
+                Console.WriteLine();
             }
+        }
+
+        private int MapTree(Node n, string[] visualArray, int level)
+        {
+            if (n == null) return 0;
+
+            int maxlevel = 1;
+
+            visualArray[level] = n.Word;
+            level *= 2;
+            maxlevel = Math.Max(level, MapTree(n.Left, visualArray, level));
+            maxlevel = Math.Max(level, MapTree(n.Right, visualArray, level + 1));
+
+            return maxlevel;
         }
     }
 }
